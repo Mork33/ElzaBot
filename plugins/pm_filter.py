ae1c19f8c92233b667dc042f637c8a3d69856794
kichu <kichuxpro@outlook.com>
2025-12-04 14:05:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2090,7 +2090,6 @@ async def auto_filter(client, msg, spoll=False):
             for file_num, file in enumerate(files, start=1):
                 cap += f"\n\n<b>{file_num}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>{get_size(file.file_size)} | {clean_filename(file.file_name)}</a></b>"
     else:
-    # Select a random Christmas greeting
     CHRISTMAS_GREETINGS = random.choice(CHRISTMAS_GREETINGS_LIST)
     
     if settings.get('button'):
